fdc23b93da80cdd04ff986cb127967865dad07ad
Vladimir008 <vladimir.podkidyshev@gmail.com>
2023-06-13 22:33:55 +0300

    add service

---
+++ src/main/java/com/podkidyshev/presentation/service/TelegramBot.java
@@ -0,0 +1,52 @@
+package com.podkidyshev.presentation.service;
+
+import com.podkidyshev.presentation.config.BotConfig;
+import com.podkidyshev.presentation.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get a welcome message!"));
+        listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/setting", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            System.out.println("Error setting bot's command list " + e.getMessage());
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
